62c830ac80db4a2aff3574f28aea4fcc349ce75a
jamie <zomb16jam9@gmail.com>
2022-07-31 10:14:51 +0800

    feat: get posts api data, get router paramMap variable

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
+import { Article } from 'src/app/interfaces/article';
+import { PostService } from 'src/app/post.service';
 
 @Component({
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
+  article?: Article | null;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private postService: PostService
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((param) => {
+      const id = param.get('id');
+      if (id) {
+        this.postService
+          .getArticle(id)
+          .pipe(map((response) => response.article))
+          .subscribe((article) => {
+            this.article = article;
+            console.log('article: ', article);
+          });
+      }
+    });
   }
-
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { Article } from '../interfaces/article';
+import { Articles } from '../interfaces/articles';
+import { PostService } from '../post.service';
 
 @Component({
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
-  constructor() {}
+  articles: Article[] = [];
 
-  ngOnInit(): void {}
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.postService
+      .getArticles()
+      .pipe(map((response: Articles) => response.articles))
+      .subscribe((data) => {
+        this.articles = data;
+      });
+  }
 }
